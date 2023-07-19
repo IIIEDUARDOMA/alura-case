b326cbc5dba7ccd360c008baaf6dbe5060507338
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-07-19 20:33:10 -0300

    First commit: Create pages Home, faq and  firt component

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Alura Case - Pagina de perguntas "FAQ"</h1>
+      <Link href="/">Ir para Home</Link>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Alura case - Home</h1>
+      <Link href="/faq">Ir para a FAQ</Link>
+    </div>
+  );
+}
