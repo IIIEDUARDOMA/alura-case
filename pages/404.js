986c3a4e55840b0ae0bcb84e2856bd1d0c0a141c
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-07-19 21:56:25 -0300

    Second commit: Create page 404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>Você se perdeu e caiu na pagina 404 :o</h1>
+      <Link href="/">Ir para Home</Link>
+    </div>
+  );
+}
